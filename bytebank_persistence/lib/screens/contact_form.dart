cd30eb3dd4a18d185c7119f633424d186991efbd
Dieinimy <dieinimy@gmail.com>
2020-04-10 14:45:41 -0300

    Iniciada implementação de cadastro de um novo contato

--- bytebank_persistence/lib/screens/contact_form.dart
+++ bytebank_persistence/lib/screens/contact_form.dart
@@ -1,6 +1,17 @@
+import 'package:bytebank_persistence/models/contact.dart';
 import 'package:flutter/material.dart';
 
-class ContactForm extends StatelessWidget {
+class ContactForm extends StatefulWidget {
+  @override
+  _ContactFormState createState() => _ContactFormState();
+}
+
+class _ContactFormState extends State<ContactForm> {
+  final TextEditingController _nameController = TextEditingController();
+
+  final TextEditingController _accountNumberController =
+      TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -12,12 +23,14 @@ class ContactForm extends StatelessWidget {
         child: Column(
           children: <Widget>[
             TextField(
+              controller: _nameController,
               decoration: InputDecoration(labelText: 'Full name'),
               style: TextStyle(fontSize: 24.0),
             ),
             Padding(
               padding: const EdgeInsets.only(top: 8.0),
               child: TextField(
+                controller: _accountNumberController,
                 decoration: InputDecoration(labelText: 'Account number'),
                 style: TextStyle(fontSize: 24.0),
                 keyboardType: TextInputType.number,
@@ -29,7 +42,13 @@ class ContactForm extends StatelessWidget {
                 width: double.maxFinite,
                 child: RaisedButton(
                   child: Text('Create'),
-                  onPressed: () {},
+                  onPressed: () {
+                    final String name = _nameController.text;
+                    final int accountNumber =
+                        int.tryParse(_accountNumberController.text);
+                    final Contact newContact = Contact(name, accountNumber);
+                    Navigator.pop(context, newContact);
+                  },
                 ),
               ),
             )
