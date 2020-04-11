4ff067d13ef0c00627558b99aea9ff32eb1c7724
Dieinimy <dieinimy@gmail.com>
2020-04-11 13:58:34 -0300

    Incluindo opção de salvar no banco de dados

--- bytebank_persistence/lib/screens/contact_form.dart
+++ bytebank_persistence/lib/screens/contact_form.dart
@@ -1,3 +1,4 @@
+import 'package:bytebank_persistence/database/app_database.dart';
 import 'package:bytebank_persistence/models/contact.dart';
 import 'package:flutter/material.dart';
 
@@ -47,7 +48,7 @@ class _ContactFormState extends State<ContactForm> {
                     final int accountNumber =
                         int.tryParse(_accountNumberController.text);
                     final Contact newContact = Contact(0, name, accountNumber);
-                    Navigator.pop(context, newContact);
+                    save(newContact).then((id) => Navigator.pop(context));
                   },
                 ),
               ),
