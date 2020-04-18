43d0f4afc75564753f39d63b9c1619c990e96d2d
Dieinimy <dieinimy@gmail.com>
2020-04-18 11:24:36 -0300

    Criado função para retornar um GET do endpoint de transactions e convertando para nosso objeto Transaction

--- bytebank_api/lib/http/webclient.dart
+++ bytebank_api/lib/http/webclient.dart
@@ -1,3 +1,8 @@
+import 'dart:convert';
+
+import 'package:bytebank/models/contact.dart';
+import 'package:bytebank/models/transaction.dart';
+import 'package:bytebank/screens/transactions_list.dart';
 import 'package:http/http.dart';
 import 'package:http_interceptor/http_interceptor.dart';
 
@@ -22,10 +27,24 @@ class LoggingInterceptor implements InterceptorContract {
   }
 }
 
-void findAll() async {
-  final Client client =
-      HttpClientWithInterceptor.build(interceptors: [LoggingInterceptor()]);
-  final Response response = await client.get(
-    'http://192.168.0.103:8080/transactions',
+Future<List<Transaction>> findAll() async {
+  final Client client = HttpClientWithInterceptor.build(
+    interceptors: [LoggingInterceptor()],
   );
+  final Response response = await client.get('http://192.168.0.103:8080/transactions');
+  final List<dynamic> decodedJson = jsonDecode(response.body);
+  final List<Transaction> transactions = List();
+  for(Map<String, dynamic> transactionJson in decodedJson){
+    final Map<String, dynamic> contactJson = transactionJson['contact'];
+    final Transaction transaction = Transaction(
+      transactionJson['value'],
+      Contact(
+        0,
+        contactJson['name'],
+        contactJson['accountNumber'],
+      ),
+    );
+    transactions.add(transaction);
+  }
+  return transactions;
 }
