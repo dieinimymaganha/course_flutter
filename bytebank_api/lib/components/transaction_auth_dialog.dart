0698be146fc5f6821f6ea1647cbfba472ca0a1e1
Dieinimy <dieinimy@gmail.com>
2020-04-19 14:59:04 -0300

    Criando componente Dialog

---
+++ bytebank_api/lib/components/transaction_auth_dialog.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+class TransactionaAuthDialog extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      title: Text('Authenticate'),
+      content: TextField(
+        obscureText: true,
+        maxLength: 4,
+        decoration: InputDecoration(
+          border: OutlineInputBorder()
+        ),
+        textAlign: TextAlign.center,
+        keyboardType: TextInputType.number,
+        style: TextStyle(fontSize: 64,letterSpacing: 32),
+      ),
+
+      actions: <Widget>[
+        FlatButton(
+          onPressed: () {
+            print('Cancel');
+          },
+          child: Text('Cancel'),
+        ),
+        FlatButton(
+          onPressed: () {
+            print('Confirm');
+          },
+          child: Text('Confirm'),
+        ),
+      ],
+    );
+  }
+}
