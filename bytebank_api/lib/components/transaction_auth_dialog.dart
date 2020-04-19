e162ec33770fc1b4c560b56f4ac3243d39873fe3
Dieinimy <dieinimy@gmail.com>
2020-04-19 15:27:50 -0300

    Realizado alterações na Dialog para fazer confirmação da transferência

--- bytebank_api/lib/components/transaction_auth_dialog.dart
+++ bytebank_api/lib/components/transaction_auth_dialog.dart
@@ -1,31 +1,38 @@
 import 'package:flutter/material.dart';
 
-class TransactionaAuthDialog extends StatelessWidget {
+class TransactionAuthDialog extends StatefulWidget {
+  final Function(String password) onConfirm;
+
+  TransactionAuthDialog({@required this.onConfirm});
+
+  @override
+  _TransactionAuthDialog createState() => _TransactionAuthDialog();
+}
+
+class _TransactionAuthDialog extends State<TransactionAuthDialog> {
+  final TextEditingController _passwordController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return AlertDialog(
       title: Text('Authenticate'),
       content: TextField(
+        controller: _passwordController,
         obscureText: true,
         maxLength: 4,
-        decoration: InputDecoration(
-          border: OutlineInputBorder()
-        ),
+        decoration: InputDecoration(border: OutlineInputBorder()),
         textAlign: TextAlign.center,
         keyboardType: TextInputType.number,
-        style: TextStyle(fontSize: 64,letterSpacing: 32),
+        style: TextStyle(fontSize: 64, letterSpacing: 24),
       ),
-
       actions: <Widget>[
         FlatButton(
-          onPressed: () {
-            print('Cancel');
-          },
+          onPressed: () => Navigator.pop(context),
           child: Text('Cancel'),
         ),
         FlatButton(
           onPressed: () {
-            print('Confirm');
+            widget.onConfirm(_passwordController.text);
           },
           child: Text('Confirm'),
         ),
