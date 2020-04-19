7b16bbc1c2de132c5b180fb428fbaf96258496b4
Dieinimy <dieinimy@gmail.com>
2020-04-19 16:12:38 -0300

    Refatorando o código, e criando MAP de error

--- bytebank_api/lib/http/webclients/transaction_webclient.dart
+++ bytebank_api/lib/http/webclients/transaction_webclient.dart
@@ -20,14 +20,18 @@ class TransactionWebClient {
         headers: {'Content-type': 'application/json', 'password': password},
         body: transactionJson);
 
-    if (response.statusCode == 400) {
-      throw Exception('there was an error submitting transaction');
+    if (response.statusCode == 200) {
+      return Transaction.fromJson(jsonDecode(response.body));
     }
 
-    if (response.statusCode == 401) {
-      throw Exception('authentication fail');
-    }
-
-    return Transaction.fromJson(jsonDecode(response.body));
+    _throwHttpError(response.statusCode);
   }
+
+  void _throwHttpError(int statusCode) =>
+      throw Exception(_statusCodeResponses[statusCode]);
+
+  static final Map<int, String> _statusCodeResponses = {
+    400: 'there was an error submitting transaction',
+    401: 'authentication fail'
+  };
 }
