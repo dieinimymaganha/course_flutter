4efe47a3eb542fe922dde5e05ccfe9562e1f4fba
Dieinimy <dieinimy@gmail.com>
2020-04-19 12:53:11 -0300

    Refatorando
    Fazendo as chamadas para os conversores

--- bytebank_api/lib/http/webclients/transaction_webclient.dart
+++ bytebank_api/lib/http/webclients/transaction_webclient.dart
@@ -17,24 +17,13 @@ class TransactionWebClient {
     final List<dynamic> decodedJson = jsonDecode(response.body);
     final List<Transaction> transactions = List();
     for (Map<String, dynamic> transactionJson in decodedJson) {
-      final Map<String, dynamic> contactJson = transactionJson['contact'];
-      final Transaction transaction = Transaction(
-        transactionJson['value'],
-        Contact(
-          0,
-          contactJson['name'],
-          contactJson['accountNumber'],
-        ),
-      );
-      transactions.add(transaction);
+      transactions.add(Transaction.fromJson(transactionJson));
     }
     return transactions;
   }
 
   Future<Transaction> save(Transaction transaction) async {
-    Map<String, dynamic> transactionMap = _toMap(transaction);
-
-    final String transactionJson = jsonEncode(transactionMap);
+    final String transactionJson = jsonEncode(transaction.toJson());
 
     final Response response = await client.post(baseUrl,
         headers: {'Content-type': 'application/json', 'password': '1000'},
@@ -45,25 +34,6 @@ class TransactionWebClient {
 
   Transaction _toTransaction(Response response) {
     Map<String, dynamic> json = jsonDecode(response.body);
-    final Map<String, dynamic> contactJson = json['contact'];
-    return Transaction(
-      json['value'],
-      Contact(
-        0,
-        contactJson['name'],
-        contactJson['accountNumber'],
-      ),
-    );
-  }
-
-  Map<String, dynamic> _toMap(Transaction transaction) {
-    final Map<String, dynamic> transactionMap = {
-      'value': transaction.value,
-      'contact': {
-        'name': transaction.contact.name,
-        'accountNumber': transaction.contact.accountNumber
-      }
-    };
-    return transactionMap;
+    return Transaction.fromJson(json);
   }
 }
