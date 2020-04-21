f2b1535a77d9f45ec75e5acd6ba8b188cdf98dc3
Dieinimy <dieinimy@gmail.com>
2020-04-21 12:06:00 -0300

    Removendo metodo featureMatcher

--- flutter-webapi-2-aula-5/test/dashboard_widget_test.dart
+++ flutter-webapi-2-aula-5/test/dashboard_widget_test.dart
@@ -2,6 +2,8 @@ import 'package:bytebank/screens/dashboard.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
+import 'matchers.dart';
+
 void main() {
   testWidgets('Should display the main image when the dashboard is open',
       (WidgetTester tester) async {
@@ -30,9 +32,4 @@ void main() {
   });
 }
 
-bool featureItemMatcher(Widget widget, String name, IconData icon) {
-  if (widget is FeatureItem) {
-    return widget.name == name && widget.icon == icon;
-  }
-  return false;
-}
+
