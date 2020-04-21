d046bc60f8f6c00d2532dc5a3a31a9a158a4406e
Dieinimy <dieinimy@gmail.com>
2020-04-21 13:35:15 -0300

    Teste Fluxo
    Dando continuidade na criação do teste para Transfer

--- flutter-webapi-2-aula-5/test/flows/transfer_flow.dart
+++ flutter-webapi-2-aula-5/test/flows/transfer_flow.dart
@@ -1,7 +1,11 @@
 import 'package:bytebank/main.dart';
+import 'package:bytebank/models/contact.dart';
 import 'package:bytebank/screens/contacts_list.dart';
 import 'package:bytebank/screens/dashboard.dart';
+import 'package:bytebank/screens/transaction_form.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
 import '../mocks/mocks.dart';
 import 'actions.dart';
 
@@ -14,11 +18,34 @@ void main() {
     final dashboard = find.byType(Dashboard);
     expect(dashboard, findsOneWidget);
 
+
+    when(mockContactDao.findAll()).thenAnswer((invocation) async {
+      debugPrint('name invocation ${invocation.memberName}');
+      return [Contact(0, 'Alex', 1000)];
+    });
+
     await clickOnTheTransferFeatureItem(tester);
     await tester.pumpAndSettle();
 
     final contactsList = find.byType(ContactsList);
     expect(contactsList, findsOneWidget);
+
+    verify(mockContactDao.findAll()).called(1);
+
+    final contactItem = find.byWidgetPredicate((widget) {
+      if (widget is ContactItem) {
+        return widget.contact.name == 'Alex' &&
+            widget.contact.accountNumber == 1000;
+      }
+      return false;
+    });
+
+    expect(contactItem, findsOneWidget);
+    await tester.tap(contactItem);
+    await tester.pumpAndSettle();
+
+    final transactionForm = find.byType(TransactionForm);
+    expect(transactionForm, findsOneWidget);
+
   });
 }
-
